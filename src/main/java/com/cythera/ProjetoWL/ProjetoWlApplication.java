91e5f90729160af574680da018cab62c9c9a8800
Walter Lemos <walterlemos31@gmail.com>
2020-06-24 09:58:15 -0300

    Projeto Criado

---
+++ src/main/java/com/cythera/ProjetoWL/ProjetoWlApplication.java
@@ -0,0 +1,13 @@
+package com.cythera.ProjetoWL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cythera/ProjetoWL/ProjetoWlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cythera.ProjetoWL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
