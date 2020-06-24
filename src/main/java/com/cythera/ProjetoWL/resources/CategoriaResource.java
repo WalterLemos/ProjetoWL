8aba9a3a7173b74c7f9fc4979ded2b66baf902cd
Walter Lemos <walterlemos31@gmail.com>
2020-06-24 11:19:21 -0300

    Classe de Dominio Categoria Testada

--- src/main/java/com/cythera/ProjetoWL/resources/CategoriaResource.java
+++ src/main/java/com/cythera/ProjetoWL/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.cythera.ProjetoWL.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cythera.ProjetoWL.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -12,9 +17,17 @@ public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
 	
-	public String lista() {
+	public List<Categoria> lista() {
 		
-		return "Rest está funcionando!";
+		
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 		
 		
 	}
