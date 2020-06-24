a3107ebe96f00e4d837fe44057f96acee27eb403
Walter Lemos <walterlemos31@gmail.com>
2020-06-24 15:38:42 -0300

    Instacia Objeto

--- src/main/java/com/cythera/ProjetoWL/ProjetoWlApplication.java
+++ src/main/java/com/cythera/ProjetoWL/ProjetoWlApplication.java
@@ -1,13 +1,37 @@
 package com.cythera.ProjetoWL;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cythera.ProjetoWL.domain.Categoria;
+import com.cythera.ProjetoWL.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetoWlApplication {
+public class ProjetoWlApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoWlApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null,"INFORMÁTICA");
+		Categoria cat2 = new Categoria(null,"ESCRITÓRIO");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
+ 
+		
 	}
 
 }
